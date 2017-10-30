830af8ef5751f2e1e156936274c79edff169dba6
channing.liu <channing.liu@vipshop.com>
2017-10-30 10:47:40 +0800

    与react-native-web的babel-loader结合，需要使用import代替require

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-var parseCss = require('gulp-react-native-stylesheet-css/lib/parseCss');
+const parseCss = require('gulp-react-native-stylesheet-css/lib/parseCss');
 
 module.exports = function(source) {
-
-	var style = parseCss(source.replace(/\r?\n|\r/g, ""));
-
-	var prefix = "", suffix = "";
-	prefix = "var React = require('react-native');\nmodule.exports = React.StyleSheet.create(";
-	suffix = ");";
-	var content = prefix + style + suffix
-    return content;
+    return `import {StyleSheet} from 'react-native';export default StyleSheet.create(${parseCss(source.replace(/\r?\n|\r/g, ""))});`;
 };
